4e985e8fdcaf9a401cbb93c63aaeba39083ae442
sahil <sahilofficial17@gmail.com>
2021-02-05 09:12:00 +0530

    author

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World . <br></br>
+          Author : Sahil 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Sahil1709"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github
         </a>
       </header>
     </div>
